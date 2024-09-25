74d6ce70ad923364df4dbba35e7c1140fd21fa72
Jesus Blanco Miguez <119857344+eluiui@users.noreply.github.com>
2024-09-25 12:21:08 +0200

    Update Exercise02.java

--- src/main/java/com/campusdual/classroom/Exercise02.java
+++ src/main/java/com/campusdual/classroom/Exercise02.java
@@ -10,6 +10,7 @@ public class Exercise02 {
         a=r*r*Math.PI;
         System.out.println("Area: " + a);
 
+        
         // TODO: ↓ Fórmula de la longitud de una circunferencia: l = 2 * radio * Número PI
         l=2*r*Math.PI;
         System.out.println("Longitud:  " + l);
